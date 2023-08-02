6e7c7dd89d94aea7a7559a4d64f60010ae527e43
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-08-02 17:46:51 +0200

    update

--- src/database.ts
+++ src/database.ts
@@ -4,38 +4,36 @@ import { Pool } from 'pg';
 dotenv.config();
 
 const {
-    POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-    POSTGRES_TEST_DB,
-    ENV,
-  } = process.env
-
-  
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_TEST_DB,
+  ENV,
+} = process.env
 let client: any;
 console.log(ENV)
 
 console.log(`Connected to ${POSTGRES_DB} database.`);
 
 
-if(ENV === 'test') {
-    client = new Pool({
-      host: POSTGRES_HOST,
-      database: POSTGRES_DB,
-      user: POSTGRES_USER,
-      password: POSTGRES_PASSWORD,
-    })
-  }
-  
-  if(ENV === 'dev') {
-    client = new Pool({
-      host: POSTGRES_HOST,
-      database: POSTGRES_DB,
-      user: POSTGRES_USER,
-      password: POSTGRES_PASSWORD,
-    })
-  }
- 
+if (ENV === 'test') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  })
+}
+
+if (ENV === 'dev') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  })
+}
+
 
 export default client;
