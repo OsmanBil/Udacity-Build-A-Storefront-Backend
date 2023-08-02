e04db7f7ec06fcacd2b0efdcc7f738543dc1da1a
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-08-02 16:42:44 +0200

    update

--- src/dist/server.js
+++ src/dist/server.js
@@ -5,15 +5,18 @@ var body_parser_1 = require("body-parser");
 var users_1 = require("./handlers/users");
 var products_1 = require("./handlers/products");
 var orders_1 = require("./handlers/orders");
-var app = express_1["default"]();
-var address = "0.0.0.0:3000";
-app.use(body_parser_1["default"].json());
+var app = express_1["default"](); // Create an Express application (App)
+var address = "0.0.0.0:3000"; // The address and port on which the server will run
+app.use(body_parser_1["default"].json()); // Use the bodyParser middleware to parse the request body if it is in JSON format
+// Define a route handler for the main endpoint ('/') of the server
 app.get('/', function (req, res) {
     res.send('Main Route is working');
 });
+// Connect the user routes handler, product routes handler, and order routes handler to the Express app
 users_1["default"](app);
 products_1["default"](app);
 orders_1["default"](app);
+// Start the server and make it listen for incoming connections on port 3000
 app.listen(3000, function () {
     console.log("starting app on: " + address);
 });
--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,22 @@ import users_routes from './handlers/users'
 import products_routes from './handlers/products'
 import order_routes from './handlers/orders'
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: express.Application = express() // Create an Express application (App)
+const address: string = "0.0.0.0:3000" // The address and port on which the server will run
 
-app.use(bodyParser.json())
+app.use(bodyParser.json()) // Use the bodyParser middleware to parse the request body if it is in JSON format
 
+// Define a route handler for the main endpoint ('/') of the server
 app.get('/', function (req: Request, res: Response) {
     res.send('Main Route is working')
 })
 
+// Connect the user routes handler, product routes handler, and order routes handler to the Express app
 users_routes(app)
 products_routes(app)
 order_routes(app)
 
+// Start the server and make it listen for incoming connections on port 3000
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
 })
